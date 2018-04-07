3faf3c621e7949726454518ff6966119d85c89b5
Simon Albinsson <giunk@zxz.se>
2018-04-07 22:23:17 +0000

    adds basic setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Learn more: https://github.com/zinob/RPI_SGP30
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sgp30',
+    description='Library for reading data from the sensiron SGP30' 
+    version='0.1.0',
+    long_description=readme,
+    author='Kenneth Reitz',
+    author_email='pipmon@zinob.se',
+    url='https://github.com/zinob/RPI_SGP30',
+    license=license,
+    packages=find_packages(exclude=('tests'))
+)
