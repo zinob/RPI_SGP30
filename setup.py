56d18292053563511f5e9e199dc0a13c34d7194e
Simon Albinsson <giunk@zxz.se>
2018-04-23 19:18:44 +0000

    setup.py: Taggs for currently supported python versions

--- setup.py
+++ setup.py
@@ -22,8 +22,12 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
     ],
     keywords='sgp30 i2c smbus smbus2',
-
 )
