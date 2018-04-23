967aac5289b81edb37b001a2013d180901a6e01c
Simon Albinsson <giunk@zxz.se>
2018-04-23 08:31:54 +0000

    setup.py: changes to license, hopefully fixes pypi

--- setup.py
+++ setup.py
@@ -11,12 +11,19 @@ with open('LICENSE') as f:
 setup(
     name='sgp30',
     description='Library for reading data from the sensiron SGP30',
-    version='0.1.2',
+    version='0.1.3',
     long_description=readme,
     author='Simon Albinsson',
     author_email='pipmon@zinob.se',
     url='https://github.com/zinob/RPI_SGP30',
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests')),
-    install_requires=['smbus2']
+    install_requires=['smbus2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ],
+    keywords='sgp30 i2c smbus smbus2',
+
 )
